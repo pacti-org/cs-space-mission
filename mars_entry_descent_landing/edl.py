df64caa580279cba8ade1a526564445d90c81d25
Nicolas Rouquette <nfr@jpl.nasa.gov>
2023-03-24 21:35:27 -0700

    refactored to use media

--- mars_entry_descent_landing/edl.py
+++ mars_entry_descent_landing/edl.py
@@ -13,18 +13,3 @@ skycrane_v_exit = 0
 
 skycrane_t_entry = 398.0
 skycrane_t_exit = 410.0
-
-# For converting large images to base64, use: https://www.base64encoder.io/image-to-base64-converter/
-# The strings below correspond to "Base64 Encoded String"
-
-# The format for the inline figures as follows:
-#
-# variable=""  # noqa: E501
-
-with open("images.json") as f:
-    file_data = json.load(f)
-
-figure_m2020_edl_timeline_segmented = file_data["figure_m2020_edl_timeline_segmented"]
-figure_segment_contracts_page1 = file_data["figure_segment_contracts_page1"]
-figure_segment_contracts_page2 = file_data["figure_segment_contracts_page2"]
-figure_segment_contracts_page3 = file_data["figure_segment_contracts_page3"]
